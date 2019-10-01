4b30fde9dc00974101c0d18f08b7f94c45587c8a
sfsavard <45376083+sfsavard@users.noreply.github.com>
2019-09-30 20:33:39 -0400

    projectile physics

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -9,7 +9,19 @@ public class Slingshot : MonoBehaviour {
 
     // Place class variables here
 
+    // fields set in the Unity Inspector pane
+    [Header("Set in Inspector")]                                           // a
+    public GameObject prefabProjectile;
+    public float velocityMult = 8f;                                 // a
+
+    // fields set dynamically
+    [Header("Set Dynamically")]                                            // a
+
     public GameObject launchPoint;
+    public Vector3 launchPos;                                         // b
+    public GameObject projectile;                                      // b
+    public bool aimingMode;                                         // b
+    private Rigidbody projectileRigidbody;                             // a
 
     void OnMouseEnter()
     {
@@ -23,6 +35,56 @@ public class Slingshot : MonoBehaviour {
         launchPoint.SetActive(false);                                          // b
     }
 
+    void OnMouseDown()
+    {                                                    // d
+        // The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+        // Instantiate a Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        // Start it at the launchPoint
+        projectile.transform.position = launchPos;
+        // Set it to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+        // Set it to isKinematic for now
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();               // a
+        projectileRigidbody.isKinematic = true;                                   // a
+    }
+
+    void Update()
+    {
+        // If Slingshot is not in aimingMode, don't run this code
+        if (!aimingMode) return;                                                   // b
+
+        // Get the current mouse position in 2D screen coordinates
+        Vector3 mousePos2D = Input.mousePosition;                                  // c
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        // Find the delta from the launchPos to the mousePos3D
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        // Limit mouseDelta to the radius of the Slingshot SphereCollider         // d
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        // Move the projectile to this new position
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0))
+        {                                        // e
+            // The mouse has been released
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            projectile = null;
+        }
+    }
+
+
 
 
     private void Awake()
@@ -30,15 +92,11 @@ public class Slingshot : MonoBehaviour {
         Transform launchPointTrans = transform.Find("LaunchPoint");            // a
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);                                         // b
+        launchPos = launchPointTrans.position;                              // c
     }
 
 
 
 
-    private void Update()
-    {
- 
-
-
-    }
+    
 }
