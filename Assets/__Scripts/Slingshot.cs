ea9254416fed02ddfb7f59b448ecb70aecbb49a3
sfsavard <45376083+sfsavard@users.noreply.github.com>
2019-09-30 20:09:55 -0400

    halo effect

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 // YOU must implement the Slingshot
 
 public class Slingshot : MonoBehaviour {
-  
 
-// Place class variables here
+
+    // Place class variables here
+
+    public GameObject launchPoint;
+
+    void OnMouseEnter()
+    {
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);                                           // b
+    }
+
+    void OnMouseExit()
+    {
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);                                          // b
+    }
 
 
 
     private void Awake()
     {
-
+        Transform launchPointTrans = transform.Find("LaunchPoint");            // a
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);                                         // b
     }
+
+
 
 
     private void Update()
